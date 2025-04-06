d31befe5e541d721bfa59920776c7dfb90bf56cc
david-swift <haenerdavid@icloud.com>
2025-04-06 12:16:52 +0200

    Update to Swift 6.1

--- setup-offline.sh
+++ setup-offline.sh
@@ -3,8 +3,8 @@ mkdir .build/repositories
 cd .build/repositories
 mkdir ./swift-argument-parser-54a11a8d
 cp -r ../checkouts/swift-argument-parser/.git/* ./swift-argument-parser-54a11a8d
-mkdir ./swift-cmark-7ac81116
-cp -r ../checkouts/swift-cmark/.git/* ./swift-cmark-7ac81116
+mkdir ./swift-cmark-8e53c1c0
+cp -r ../checkouts/swift-cmark/.git/* ./swift-cmark-8e53c1c0
 mkdir ./swift-markdown-6b045e7c
 cp -r ../checkouts/swift-markdown/.git/* ./swift-markdown-6b045e7c
 mkdir ./swift-syntax-e1f983d3
